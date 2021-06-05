04b2a798ac882aaf9425078b148f10abe68a762a
vishal <vjaisknit@gmail.com>
2021-06-05 19:35:04 +0530

    Initail Commit

---
+++ Vish_Tech_Ecom/Vish_Tech_Ecom.API/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vish_Tech_Ecom.Core.Data;
+using Vish_Tech_Ecom.Core.Entities;
+
+namespace Vish_Tech_Ecom.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ProductController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+           // return _appDbContext.Products.ToList();
+           return NoContent();
+        }
+
+        [Route("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            return _appDbContext.Products.FirstOrDefault(a => a.Id == id);
+        }
+    }
+}
---
+++ Vish_Tech_Ecom/Vish_Tech_Ecom.Core/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vish_Tech_Ecom.Core.Entities;
+
+namespace Vish_Tech_Ecom.Core.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { set; get; }
+    }
+}
